d3e12e2051009b6fe629875df5d8182ae47fd6b2
ACM-Anik <anikmojumder5252@gmail.com>
2023-05-20 22:22:01 +0600

    Email by GET method added

--- index.js
+++ index.js
@@ -42,6 +42,14 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/myToys/:email', async(req, res) => {
+            const email = req.params.email;
+            console.log(email);
+            // // const query = 
+            const result = await toysCollection.find({seller_email: email}).toArray();
+            res.send(result);
+        })
+
         app.post('/actionToys', async(req, res) => {
             const newToy = req.body;
             console.log(newToy);
